5a5ac801aac1cbaf4349c235e1e06fd0f18aee33
Jason-Lan <apple@appledeiMac.local>
2017-12-04 14:31:35 +0800

    第一版

---
+++ ShakeIt-ShakeIt/ShakeIt-ShakeIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShakeIt-ShakeIt
+//
+//  Created by apple on 2017/12/4.
+//  Copyright © 2017年 Lan's Personal Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShakeIt-ShakeIt/ShakeIt-ShakeIt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShakeIt-ShakeIt
+//
+//  Created by apple on 2017/12/4.
+//  Copyright © 2017年 Lan's Personal Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
