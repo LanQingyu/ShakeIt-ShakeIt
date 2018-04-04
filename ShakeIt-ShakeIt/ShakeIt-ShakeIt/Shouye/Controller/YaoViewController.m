01f2247e73fae635d4e10c5d0a3ccb7d313348b1
Jason-Lan <apple@appledeiMac.local>
2018-04-04 09:53:02 +0800

    111

--- ShakeIt-ShakeIt/ShakeIt-ShakeIt/Shouye/Controller/YaoViewController.m
+++ ShakeIt-ShakeIt/ShakeIt-ShakeIt/Shouye/Controller/YaoViewController.m
@@ -9,8 +9,17 @@
 #import "YaoViewController.h"
 #import "Header.h"
 @interface YaoViewController ()
+{
+    NSInvocation * invo;
+}
 
 @property(nonatomic, strong)UIScrollView *scrollView;
+@property(nonatomic, assign)NSInteger i;
+@property(nonatomic, strong)UIBarButtonItem *beginItem;
+@property(nonatomic, strong)UIBarButtonItem *stopItem;
+@property(nonatomic, strong)NSTimer *timer;
+
+
 
 @end
 
@@ -18,16 +27,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"%@", _dataArray);
-    
-     [self.navigationItem setRightBarButtonItem:[[UIBarButtonItem alloc]initWithTitle:@"开始" style:UIBarButtonItemStylePlain target:self action:@selector(begin)]];
+    self.navigationItem.title = @"摇起来";
+    _beginItem = [[UIBarButtonItem alloc]initWithTitle:@"开始" style:UIBarButtonItemStylePlain target:self action:@selector(begin)];
+    _stopItem = [[UIBarButtonItem alloc]initWithTitle:@"停" style:UIBarButtonItemStylePlain target:self action:@selector(begin)];
+    self.navigationItem.rightBarButtonItem = _beginItem;
+    _i = 1;
+    invo = [NSInvocation invocationWithMethodSignature:[[self class] instanceMethodSignatureForSelector:@selector(init)]];
+    [invo setTarget:self];
+    [invo setSelector:@selector(change)];
+    //加入主循环池中
     
 }
 -(void)begin
 {
-    NSLog(@"开始");
-    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.01   target:self selector:@selector(change) userInfo:nil repeats:YES];
     
+//    _timer = [NSTimer scheduledTimerWithTimeInterval:0.001   target:self selector:@selector(change) userInfo:nil repeats:YES];
+//    _timer = [NSTimer timerWithTimeInterval:0.001 target:self selector:@selector(change) userInfo:nil repeats:YES];
+    if (_i == 1) {
+        _timer = [NSTimer timerWithTimeInterval:0.001 invocation:invo repeats:YES];
+        [[NSRunLoop mainRunLoop]addTimer:_timer forMode:NSDefaultRunLoopMode];
+        [_timer fire];
+        NSLog(@"开始");
+        self.navigationItem.rightBarButtonItem = _stopItem;
+        _i = 0;
+    }
+    else{
+        [_timer invalidate];
+        NSLog(@"停");
+        self.navigationItem.rightBarButtonItem = _beginItem;
+        _i = 1;
+    }
 }
 -(void)change{
     NSInteger i = arc4random() % _dataArray.count;
